79aa4316a1cc656e4b3e567b47a1eaca62645248
harshavardan7 <149469395+harshavardan7@users.noreply.github.com>
2025-01-30 11:20:15 +0530

    [160] [Time Beats: 29.10%] [Memory Beats: 9.19%] - LeetPush

---
+++ 160-Intersection-of-Two-Linked-Lists.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode dummy1 = headA, dummy2 = headB;
+
+        while (dummy1 != dummy2) {
+            dummy1 = dummy1 != null ? dummy1.next : headB;
+            dummy2 = dummy2 != null ? dummy2.next : headA;
+        }
+
+        return dummy1;
+    }
+}
